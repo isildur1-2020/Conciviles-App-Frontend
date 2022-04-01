e90812bb65b931614ff8aee61960d3ebf98c10a8
Esteban G <tapitarias@gmail.com>
2022-03-31 19:30:43 -0500

    add env

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const dev = "http://localhost:32456/api";
+// export const dev = "http://localhost:32456/api";
+export const dev = "https://conciviles-app.herokuapp.com/api";
 export const prod = "";
