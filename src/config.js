26f8976377aadc81901e4802aa7ecd326c56a6bf
Esteban G <tapitarias@gmail.com>
2022-04-04 18:39:12 -0500

    v3 add validation items

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-export const dev = "http://localhost:3456/api";
-// export const dev = "https://conciviles-app.herokuapp.com/api";
-export const prod = "";
+// export const dev = "http://localhost:3456/api";
+export const dev = "https://conciviles-app.herokuapp.com/api";
